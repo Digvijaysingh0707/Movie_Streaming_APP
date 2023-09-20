0b939f2bab454e40e8bd51209c1f328fa632c66c
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-09-20 18:05:03 +0530

    Added firebase

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,8 +1,11 @@
 export const checkValidateData = (email, password, name) => {
   const emailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email)
   const passwordValid = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s).{6,16}$/.test(password)
-  const nameValid = /^[A-Za-z]+(?:[-\s][A-Za-z]+)*$/.test(name)
-  if (!name) return "Name is not valid"
+  if (name) {
+    const nameValid = /^[A-Za-z]+(?:[-\s][A-Za-z]+)*$/.test(name)
+    if (!nameValid) return "Name is not valid"
+  }
+  console.log(email, password, name, 'nameValid')
   if (!emailValid) return "Email is not valid"
   if (!passwordValid) return "Password is not valid"
   return null
